70f48608ca2c7b858b212d900dbe73dd20c12158
freshervn <40496285+freshervn@users.noreply.github.com>
2020-11-17 02:19:55 +0700

    up code test chuc nang

--- javascript/chi_tiet_san_pham.js
+++ javascript/chi_tiet_san_pham.js
@@ -1,10 +1,20 @@
   
 $(document).ready(function(){
+// khi ve dien thoai thi nhay o anhr xuong cuoi
+
+// $(window).resize(function(){
+// 	if ($(window).width()<992) {
+// 		$('.product-details .container .row .img').insertAfter($('.product-details .container .row'));
+// 	}
+// });
+
 // an vao anh thi hien thi ra giua
 $('.product-details .container .img figure').click(
 	function(){
-		var image =$(this).clone();
+		var image = $(this).clone();
 		$('.product-details .container .main-img ').html(image);
 	});
 
+
 });
+
